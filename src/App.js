a29c98ddb149a135ce11955e724e36fe9ccf5e54
erik escobedo <erikscbd@gmail.com>
2022-10-06 19:51:54 -0500

    ghpages attempt two

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Container from "./pages/MainPage";
 // import Home from "./pages/Home";
 import "./App.css"
 
+var ghpages = require('gh-pages');
+
+ghpages.publish('dist', function(err) {});
+
 const App = () => {
 
 return (
