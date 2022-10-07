9c869f267d9abbb7e7fcbeeffaa237add19ced6b
erik escobedo <erikscbd@gmail.com>
2022-10-06 20:07:57 -0500

    gh pages deployed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Container from "./pages/MainPage";
 // import Home from "./pages/Home";
 import "./App.css"
 
-var ghpages = require('gh-pages');
+// var ghpages = require('gh-pages');
 
-ghpages.publish('dist', function(err) {});
+// ghpages.publish('dist', function(err) {});
 
 const App = () => {
 
